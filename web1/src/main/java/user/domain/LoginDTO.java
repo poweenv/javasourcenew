d4daa9337a77580a7121509310a61fbffd16c4a0
gwangyeol <kgkg12kg@naver.com>
2023-04-19 13:54:23 +0900

    da

---
+++ web1/src/main/java/user/domain/LoginDTO.java
@@ -0,0 +1,17 @@
+package user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoginDTO {
+	private String userId;
+	private String password;
+}
---
+++ web2/src/main/webapp/jsp/error.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isErrorPage="true"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<p>에러가 발생했습니다. 잠시후 접속해 주세요</p>
+</body>
+</html>
